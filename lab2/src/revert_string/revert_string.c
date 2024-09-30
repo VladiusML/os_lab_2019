f3268f91aebf6e273b03b9a11d1614f87d5c231b
Vladiusv <4035867-Vladiusv@users.noreply.replit.com>
2024-09-30 15:28:29 +0000

    Модифицированы файлы swap.c и revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <string.h>
 
-void RevertString(char *str)
-{
-	// your code here
+void RevertString(char *str) {
+		if (str == NULL) return;
+
+		char *start = str;
+		char *end = str + strlen(str) - 1;
+
+		while (start < end) {
+				char temp = *start;
+				*start = *end;
+				*end = temp;
+				start++;
+				end--;
+		}
 }
-
